01d4ff3c44920fef725fbf6da86153596cc00299
ytche <cheyantao@foxmail.com>
2017-12-19 10:34:17 +0800

    项目初始化

---
+++ src/main/java/net/wildwolf/tasklist/tasklist/TasklistApplication.java
@@ -0,0 +1,12 @@
+package net.wildwolf.tasklist.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistApplication.class, args);
+	}
+}
