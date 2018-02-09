51ddc8244b0d11e132bdf1c0a1925d999c940911
ytche <cheyantao@foxmail.com>
2018-02-09 16:30:20 +0800

    整合Mybatis Generate

---
+++ src/main/java/net/wildwolf/tasklist/tasklist/controller/TaskController.java
@@ -0,0 +1,17 @@
+package net.wildwolf.tasklist.tasklist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import sun.misc.Contended;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author cheyantao
+ */
+@Controller
+public class TaskController {
+
+}
